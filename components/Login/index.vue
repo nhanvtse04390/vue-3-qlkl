371ffddba3804b88978515b276b0e93f6e866467
votrongnhan <nhanvt@mor.com.vn>
2024-12-24 00:13:23 +0700

    install quasar dev

--- components/Login/index.vue
+++ components/Login/index.vue
@@ -2,11 +2,50 @@
 defineOptions({
   name: "LoginComponent"
 })
+
+const userName = ref<string>('')
+const password = ref<string>('')
+const error = ref<string | null>(null)
+
+const handleSubmit = () => {
+  console.log("login")
+}
+
 </script>
 
 <template>
-  <div>
-    ?????
+  <div class="q-pa-md" style="max-width: 400px">
+
+    <q-form
+        @submit="handleSubmit"
+        class="q-gutter-md"
+    >
+      <q-input
+          filled
+          v-model="userName"
+          label="Your name *"
+          hint="Name and surname"
+          lazy-rules
+          :rules="[ val => val && val.length > 0 || 'Please type something']"
+      />
+
+      <q-input
+          filled
+          type="number"
+          v-model="password"
+          label="Your age *"
+          lazy-rules
+          :rules="[
+          val => val !== null && val !== '' || 'Please type your age',
+          val => val > 0 && val < 100 || 'Please type a real age'
+        ]"
+      />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+      </div>
+    </q-form>
+
   </div>
 </template>
 
